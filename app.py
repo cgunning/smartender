4bdd93b61febbc2c361655098c5ea3e9c6e34672
rgunning <rgunning@kth.se>
2020-06-06 22:30:43 +0200

    fix

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ led = rgbled(17,27,22)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
     while True:
         r = randint(0,100)
         g = randint(0,100)
         b = randint(0,100) 
         led.changeto(r,g,b,0.8)
         time.sleep(2)
+    return "Hello World!"
+    
 
 @app.route("/drink")
 def drink():
@@ -26,7 +27,7 @@ def drink():
         g = randint(0,100)
         b = randint(0,100) 
         led.changeto(r,g,b,0.8)
-        time.sleep(1)
+        time.sleep(0.2)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
