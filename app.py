6b9dcea599ec2f1479ee2e8fd6d960cd984d4608
Christoffer Gunning <christoffer.gunning@kry.se>
2020-06-06 19:20:31 +0200

    Fixing install

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from classes import Led
 app = Flask(__name__)
+
+pump1 = Led.Led(10, 40)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/drink")
+def drink():
+    pump1.run(5)
+    return "Started"
 
 if __name__ == "__main__":
     app.run()
--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-python3 .m pip install -r requirements.txt --user
+python3 -m pip install -r requirements.txt --user
