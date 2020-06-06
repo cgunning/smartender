46ea4a155c1b50c21763b2e344c88e63c4f9e8f8
Christoffer Gunning <christoffer.gunning@kry.se>
2020-06-06 19:53:49 +0200

    Set pin as output

--- classes/Led.py
+++ classes/Led.py
@@ -7,6 +7,7 @@ class Led:
     def __init__(self, pin, flow_rate):
         self.pin = pin
         self.flow_rate = flow_rate
+        GPIO.setup(pin, GPIO.OUT)
     
     def run(self, time):
         GPIO.output(self.pin, GPIO.HIGH)
