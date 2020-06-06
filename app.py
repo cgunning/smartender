82f2b4e10121d9038b918443c5a86146472a8708
rgunning <rgunning@kth.se>
2020-06-06 20:49:41 +0200

    indention error

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask
 #from classes import Led
 import RPi.GPIO as GPIO
-from classes import rgbled
+from rgbled import rgbled
 from random import randint
 import time
 
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
-pump1 = rgbled(17,27,22)
+led = rgbled(17,27,22)
 
 @app.route("/")
 def hello():
-    pump1.off(0.8)
-    pump1.cleanup()
+    led.off(0.8)
+    led.cleanup()
     return "Hello World!"
 
 @app.route("/drink")
@@ -21,7 +21,7 @@ def drink():
         r = randint(0,100)
         g = randint(0,100)
         b = randint(0,100) 
-        pump1.changeto(r,g,b,0.8)
+        led.changeto(r,g,b,0.8)
         time.sleep(2)
 
 if __name__ == "__main__":
