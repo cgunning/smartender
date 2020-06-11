6042c39c8d2d463597ee0d8b81eef7d6258cbd63
Christoffer Gunning <christoffer.gunning@kry.se>
2020-06-11 19:44:06 +0200

    Adding test for pump

--- classes/Pump.py
+++ classes/Pump.py
@@ -8,16 +8,16 @@ class Pump:
     def __init__(self, pin, flowRate):
         self.pin = pin
         self.flowRate = flowRate
-        # GPIO.setup(pin, GPIO.OUT)
+        GPIO.setup(pin, GPIO.OUT)
     
     def getEstimatedPourTime(self, amount):
         return amount/self.flowRate*60
         
     def pour(self, amount):
-        # GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.pin, GPIO.HIGH)
         print("print poured " + str(amount) + " with a flow rate of " + str(self.flowRate))
         time.sleep(amount/self.flowRate*60)
-        # GPIO.output(self.pin, GPIO.LOW)
+        GPIO.output(self.pin, GPIO.LOW)
 
     def toString(self):
         return "pin: " + str(self.pin) + ", flowRate: " + str(self.flowRate)
---
+++ pumptest.py
@@ -0,0 +1,8 @@
+from classes import Pump
+
+
+pump1 = Pump.Pump(17, 100)
+pump2 = Pump.Pump(27, 100)
+pump3 = Pump.Pump(22, 100)
+
+pump1.pour(40)
